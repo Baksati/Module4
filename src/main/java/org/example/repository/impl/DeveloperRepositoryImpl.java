dc9278c889c5f95c2e22eadf9ac211466ae08c03
Baksati <baksgc@gmail.com>
2025-03-11 23:28:42 +0300

    Module4

--- src/main/java/org/example/repository/impl/DeveloperRepositoryImpl.java
+++ src/main/java/org/example/repository/impl/DeveloperRepositoryImpl.java
@@ -14,9 +14,9 @@ public class DeveloperRepositoryImpl implements DeveloperRepository {
     public List<Developer> getAll() {
         List<Developer> developers = new ArrayList<>();
         String sql = "SELECT * FROM Developer";
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            ResultSet resultSet = statement.executeQuery();
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            ResultSet resultSet = stmt.executeQuery();
             while (resultSet.next()) {
                 developers.add(mapRowToDeveloper(resultSet));
             }
@@ -26,57 +26,73 @@ public class DeveloperRepositoryImpl implements DeveloperRepository {
         return developers;
     }
 
-    @Override
-    public Developer getById(Long id) {
-        String sql = "SELECT * FROM Developer WHERE id = ?";
-        try (Connection connection = DatabaseConnection.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-            statement.setLong(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return mapRowToDeveloper(resultSet);
+        @Override
+        public Developer getById (Long id){
+            String sql = "SELECT * FROM Developer WHERE id = ?";
+            try (Connection conn = DatabaseConnection.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement(sql)) {
+                stmt.setLong(1, id);
+                ResultSet resultSet = stmt.executeQuery();
+                if (resultSet.next()) {
+                    return mapRowToDeveloper(resultSet);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            return new Developer();
         }
-        return new Developer();
-    }
 
-    @Override
-    public void save(Developer developer) {
-        String sql = "INSERT INTO Developer (firstName, lastName) VALUES (?, ?)";
-        try (Connection connection = DatabaseConnection.getConnection();
-        PreparedStatement statement = connection.prepareStatement
-                (sql, Statement.RETURN_GENERATED_KEYS)) {
-            statement.setString(1, developer.getFirstName());
-            statement.setString(2, developer.getLastName());
-            int affectedRows = statement.executeUpdate();
-            if (affectedRows > 0) {
-                try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
-                    if (generatedKeys.next()) {
-                        developer.setId(generatedKeys.getLong(1));
+        @Override
+        public void save (Developer developer){
+            String sql = "INSERT INTO Developer (firstName, lastName) VALUES (?, ?)";
+            try (Connection conn = DatabaseConnection.getConnection();
+                 PreparedStatement stmt = conn.prepareStatement
+                         (sql, Statement.RETURN_GENERATED_KEYS)) {
+                stmt.setString(1, developer.getFirstName());
+                stmt.setString(2, developer.getLastName());
+                int affectedRows = stmt.executeUpdate();
+                if (affectedRows > 0) {
+                    try (ResultSet generatedKeys = stmt.getGeneratedKeys()) {
+                        if (generatedKeys.next()) {
+                            developer.setId(generatedKeys.getLong(1));
+                        }
                     }
-                 }
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
+        }
+
+        @Override
+        public void update (Developer developer) {
+        String sql = "UPDATE Developer SET firstName = ?, lastName = ? WHERE id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+                 stmt.setString(1, developer.getFirstName());
+                 stmt.setString(2, developer.getLastName());
+                 stmt.setLong(3, developer.getId());
+                 int rowsUpdated = stmt.executeUpdate();
+                 if (rowsUpdated > 0) {
+                     System.out.println("Данные разработчика успешно обновлены");
+                 } else {
+                     System.out.println("Не удалось обновить данные разработчика");
+                 }
         } catch (SQLException e) {
-            e.printStackTrace();
+                 e.printStackTrace();
         }
     }
 
-    @Override
-    public void update(Developer developer) {
+        @Override
+        public void delete (Long id){
 
-    }
+        }
 
-    @Override
-    public void delete(Long id) {
-
+        private Developer mapRowToDeveloper (ResultSet resultSet) throws SQLException {
+            Developer developer = new Developer();
+            developer.setId(resultSet.getLong("id"));
+            developer.setFirstName(resultSet.getString("firstName"));
+            developer.setLastName(resultSet.getString("lastName"));
+            return developer;
+        }
     }
-    private Developer mapRowToDeveloper(ResultSet resultSet) throws SQLException {
-        Developer developer = new Developer();
-        developer.setId(resultSet.getLong("id"));
-        developer.setFirstName(resultSet.getString("firstName"));
-        developer.setLastName(resultSet.getString("lastName"));
-        return developer;
-    }
-}
